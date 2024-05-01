cdc2b232f387a24accded9d7ab84eaa0ff8685b7
Brayan Mejia Cuenca <mejiabrayan@protonmail.com>
2024-05-01 08:31:19 -0700

    swapped img with next cloudinary cldimage component for optimal image loading

--- src/components/MediaGallery/MediaGallery.tsx
+++ src/components/MediaGallery/MediaGallery.tsx
@@ -1,4 +1,5 @@
-"use client";
+/* eslint-disable @next/next/no-img-element */
+'use client';
 
 import { useState } from 'react';
 import Link from 'next/link';
@@ -7,11 +8,33 @@ import { Plus, X, Save } from 'lucide-react';
 import Container from '@/components/Container';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from '@/components/ui/dialog';
+
+import { CldImage } from 'next-cloudinary';
+
+
+interface CloudinaryResource {
+  height: number;
+  public_id: string;
+  secure_url: string;
+  width: number;
+}
 
 interface MediaGalleryProps {
-  resources: Array<{ id: string }>
+  resources: Array<CloudinaryResource>;
 }
 
 const MediaGallery = ({ resources }: MediaGalleryProps) => {
@@ -31,7 +54,7 @@ const MediaGallery = ({ resources }: MediaGalleryProps) => {
    */
 
   function handleOnCreationOpenChange(isOpen: boolean) {
-    if ( !isOpen ) {
+    if (!isOpen) {
       setCreation(undefined);
     }
   }
@@ -45,9 +68,9 @@ const MediaGallery = ({ resources }: MediaGalleryProps) => {
           <DialogHeader>
             <DialogTitle>Save your creation?</DialogTitle>
           </DialogHeader>
-          <DialogFooter className="justify-end sm:justify-end">
+          <DialogFooter className='justify-end sm:justify-end'>
             <Button>
-              <Save className="h-4 w-4 mr-2" />
+              <Save className='h-4 w-4 mr-2' />
               Save to Library
             </Button>
           </DialogFooter>
@@ -57,30 +80,30 @@ const MediaGallery = ({ resources }: MediaGalleryProps) => {
       {/** Management navbar presented when assets are selected */}
 
       {selected.length > 0 && (
-        <Container className="fixed z-50 top-0 left-0 w-full h-16 flex items-center justify-between gap-4 bg-white shadow-lg">
-          <div className="flex items-center gap-4">
+        <Container className='fixed z-50 top-0 left-0 w-full h-16 flex items-center justify-between gap-4 bg-white shadow-lg'>
+          <div className='flex items-center gap-4'>
             <ul>
               <li>
-                <Button variant="ghost" onClick={handleOnClearSelection}>
-                  <X className="h-6 w-6" />
-                  <span className="sr-only">Clear Selected</span>
+                <Button variant='ghost' onClick={handleOnClearSelection}>
+                  <X className='h-6 w-6' />
+                  <span className='sr-only'>Clear Selected</span>
                 </Button>
               </li>
             </ul>
             <p>
-              <span>{ selected?.length } Selected</span>
+              <span>{selected?.length} Selected</span>
             </p>
           </div>
-          <ul className="flex items-center gap-4">
+          <ul className='flex items-center gap-4'>
             <li>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button variant="ghost">
-                    <Plus className="h-6 w-6" />
-                    <span className="sr-only">Create New</span>
+                  <Button variant='ghost'>
+                    <Plus className='h-6 w-6' />
+                    <span className='sr-only'>Create New</span>
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
+                <DropdownMenuContent className='w-56'>
                   <DropdownMenuGroup>
                     <DropdownMenuItem>
                       <span>Option</span>
@@ -98,55 +121,66 @@ const MediaGallery = ({ resources }: MediaGalleryProps) => {
       <Container>
         <form>
           {Array.isArray(resources) && (
-            <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-12">
+            <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-12'>
               {resources.map((resource) => {
-                const isChecked = selected.includes(resource.id);
+                const isChecked = selected.includes(resource.public_id);
 
                 function handleOnSelectResource(checked: boolean) {
                   setSelected((prev) => {
-                    if ( checked ) {
-                      return Array.from(new Set([...(prev || []), resource.id]));
+                    if (checked) {
+                      return Array.from(
+                        new Set([...(prev || []), resource.public_id])
+                      );
                     } else {
-                      return prev.filter((id) => id !== resource.id);
+                      return prev.filter((id) => id !== resource.public_id);
                     }
                   });
                 }
 
                 return (
-                  <li key={resource.id} className="bg-white dark:bg-zinc-700">
-                    <div className="relative group">
-                      <label className={`absolute ${isChecked ? 'opacity-100' : 'opacity-0'} group-hover:opacity-100 transition-opacity top-3 left-3 p-1`} htmlFor={resource.id}>
-                        <span className="sr-only">
-                          Select Image &quot;{ resource.id }&quot;
+                  <li key={resource.public_id} className='bg-white dark:bg-zinc-700'>
+                    <div className='relative group'>
+                      <label
+                        className={`absolute ${
+                          isChecked ? 'opacity-100' : 'opacity-0'
+                        } group-hover:opacity-100 transition-opacity top-3 left-3 p-1`}
+                        htmlFor={resource.public_id}
+                      >
+                        <span className='sr-only'>
+                          Select Image &quot;{resource.public_id}&quot;
                         </span>
                         <Checkbox
-                          className={`w-6 h-6 rounded-full bg-white shadow ${isChecked ? 'border-blue-500' : 'border-zinc-200'}`}
-                          id={resource.id}
+                          className={`w-6 h-6 rounded-full bg-white shadow ${
+                            isChecked ? 'border-blue-500' : 'border-zinc-200'
+                          }`}
+                          id={resource.public_id}
                           onCheckedChange={handleOnSelectResource}
                           checked={isChecked}
                         />
                       </label>
                       <Link
-                        className={`block cursor-pointer border-8 transition-[border] ${isChecked ? 'border-blue-500' : 'border-white'}`}
-                        href="#"
+                        className={`block cursor-pointer border-8 transition-[border] ${
+                          isChecked ? 'border-blue-500' : 'border-white'
+                        }`}
+                        href='#'
                       >
-                        <img
-                          width="300"
-                          height="300"
-                          src="/icon-1024x1024.png"
-                          alt="Cloudinary Logo"
+                        <CldImage
+                          width={resource.width}
+                          height={resource.height}
+                          src={resource.public_id}
+                          alt=''
                         />
                       </Link>
                     </div>
                   </li>
-                )
+                );
               })}
             </ul>
           )}
         </form>
       </Container>
     </>
-  )
-}
+  );
+};
 
 export default MediaGallery;
